62357750f49edd6d2f8c2dd1c27d3a66db6281f6
ubugnu <ubugnu@gmail.com>
2016-12-10 17:53:26 +0100

    user-agent fix, thanks to ak.karlos@gmail.com

--- scrape_fb.sh
+++ scrape_fb.sh
@@ -50,7 +50,7 @@ function twget {
 
 function tget_name {
 	NUM="$@"
-	RES=$(twget --post-data="email=${NUM}" "https://m.facebook.com/login/identify?ctx=recover")
+	RES=$(twget --user-agent="Mozilla/5.0 (Windows NT 5.2; rv:2.0.1) Gecko/20100101 Firefox/4.0.1" --post-data="email=${NUM}" "https://m.facebook.com/login/identify?ctx=recover")
 	if [[ $RES =~ captcha ]] 
 	then 
 		echo -1
